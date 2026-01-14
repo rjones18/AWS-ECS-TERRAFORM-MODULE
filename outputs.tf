e355ab80a0b69e315a5c16df46554b222a3b7cc3
Reginald Jones <regjones16@outlook.com>
2026-01-14 10:31:13 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -14,9 +14,9 @@ output "alb_arn" {
   value = aws_lb.this.arn
 }
 
-# output "alb_dns_name" {
-#   value = aws_lb.this.dns_name
-# }
+output "alb_dns_name" {
+  value = aws_lb.this.dns_name
+}
 
 output "alb_url" {
   value = "http://${aws_lb.this.dns_name}"
