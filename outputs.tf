c532287e6de3121a8b9caa55364c54962db00774
Reginald Jones <regjones16@outlook.com>
2026-01-14 10:25:26 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -30,11 +30,6 @@ output "ecs_security_group_id" {
   value = aws_security_group.ecs.id
 }
 
-
-output "alb_dns_name" {
-  value = aws_lb.this.dns_name
-}
-
 output "alb_zone_id" {
   value = aws_lb.this.zone_id
 }
